09417d45bbbf857370fcf9761257c504b5b954fc
Matheus Carvalho <matheuso.c08@gmail.com>
2023-01-12 12:16:21 -0300

    Create the method updateList to update the recyclerView after an action. Set the 'buttonCleanList' to clean the list and call 'updateList()'. Create a method the load the shopping list with some items for test. The method checkEmptyList was updated to hide the 'buttonSendStorage' if the list is empty

--- app/src/main/java/com/home_food_storage/MainActivity.java
+++ app/src/main/java/com/home_food_storage/MainActivity.java
@@ -16,9 +16,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextSearchItem;
-    TextView textViewCleanList, textViewMessageEmptyList;
+    TextView textViewMessageEmptyList;
     RecyclerView recyclerViewItemList;
-    Button buttonNewItem;
+    Button buttonNewItem, buttonSendStorage, buttonCleanList;
     ArrayList<DtoItem> arrayItemList = new ArrayList<>();
 
     @Override
@@ -27,31 +27,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editTextSearchItem = findViewById(R.id.editTextSearchItem);
-        textViewCleanList = findViewById(R.id.textViewCleanList);
         textViewMessageEmptyList = findViewById(R.id.textViewMessageEmptyList);
         recyclerViewItemList = findViewById(R.id.recyclerViewItemList);
         buttonNewItem = findViewById(R.id.buttonNewItem);
+        buttonSendStorage = findViewById(R.id.buttonSendStorage);
+        buttonCleanList = findViewById(R.id.buttonCleanList);
 
-        ItemListAdapter adapter = new ItemListAdapter();
-        recyclerViewItemList.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        recyclerViewItemList.setAdapter(adapter);
+        carregaLista();
 
-        checkEmptyList();
+        atualizarLista();
 
         buttonNewItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                carregaLista();
+                atualizarLista();
+            }
+        });
 
+        buttonCleanList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                arrayItemList.clear();
+                atualizarLista();
             }
         });
     }
 
-    public void checkEmptyList(){
+    private void atualizarLista() {
+        ItemListAdapter adapter = new ItemListAdapter(arrayItemList);
+        recyclerViewItemList.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        recyclerViewItemList.setAdapter(adapter);
+        checkEmptyList();
+    }
+
+    private void checkEmptyList(){
         if(arrayItemList.isEmpty()){
             textViewMessageEmptyList.setVisibility(View.VISIBLE);
+            buttonSendStorage.setVisibility(View.INVISIBLE);
         }
         else{
             textViewMessageEmptyList.setVisibility(View.INVISIBLE);
+            buttonSendStorage.setVisibility(View.VISIBLE);
         }
+    }
+
+    private void carregaLista(){
+        arrayItemList.add(new DtoItem("Arroz", "Arroz", 5, 13.00, 2, true));
+        arrayItemList.add(new DtoItem("Feijao", "Feijao", 1, 5.00, 5, true));
+        arrayItemList.add(new DtoItem("Carne", "Carne", 1.5, 23.00, 1, true));
+        arrayItemList.add(new DtoItem("Miojo", "Sabor frango", 0, 4.00, 2, false));
+        arrayItemList.add(new DtoItem("Macarrão", "Espageti", 0.5, 3.00, 2, true));
+        arrayItemList.add(new DtoItem("Caixa de chocolate", null, 0, 13.00, 2, false));
     }
 }
