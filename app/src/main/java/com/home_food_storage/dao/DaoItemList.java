00d44babea9479827a1f98ab378bb368a1b63253
Matheus Carvalho <matheuso.c08@gmail.com>
2023-02-23 18:12:56 -0300

    Adjust the 'listItems' method to send the 'ITEM_ID' to the RecyclerView. Implemented the method 'UpdateItem' to update the selected item on the database. Implemented the method 'DeleteItem' to delete the selected item from the database

--- app/src/main/java/com/home_food_storage/dao/DaoItemList.java
+++ app/src/main/java/com/home_food_storage/dao/DaoItemList.java
@@ -52,6 +52,7 @@ public class DaoItemList extends SQLiteOpenHelper {
 
     public ArrayList<DtoItem> listItems(){
         String command = "SELECT " +
+                "ITEM_ID," +
                 "ITEM_NAME," +
                 "ITEM_DESCRIPTION," +
                 "ITEM_WEIGHT," +
@@ -65,17 +66,40 @@ public class DaoItemList extends SQLiteOpenHelper {
 
         while(cursor.moveToNext()){
             DtoItem dtoItem = new DtoItem();
-            dtoItem.setItemName(cursor.getString(0));
-            dtoItem.setItemDescription(cursor.getString(1));
-            dtoItem.setItemWeight(cursor.getDouble(2));
-            dtoItem.setItemPrice(cursor.getDouble(3));
-            dtoItem.setItemQuantity(cursor.getInt(4));
-            dtoItem.setItemBought(cursor.getInt(5) > 0);
+            dtoItem.setItemId(cursor.getInt(0));
+            dtoItem.setItemName(cursor.getString(1));
+            dtoItem.setItemDescription(cursor.getString(2));
+            dtoItem.setItemWeight(cursor.getDouble(3));
+            dtoItem.setItemPrice(cursor.getDouble(4));
+            dtoItem.setItemQuantity(cursor.getInt(5));
+            dtoItem.setItemBought(cursor.getInt(6) > 0);
 
             fullItemsList.add(dtoItem);
         }
 
         return fullItemsList;
+    }
+
+    public long updateItem(DtoItem dtoItem){
+        ContentValues values = new ContentValues();
+        values.put("ITEM_NAME", dtoItem.getItemName());
+        values.put("ITEM_DESCRIPTION", dtoItem.getItemDescription());
+        values.put("ITEM_WEIGHT", dtoItem.getItemWeight());
+        values.put("ITEM_PRICE", dtoItem.getItemPrice());
+        values.put("ITEM_QUANTITY", dtoItem.getItemQuantity());
+        values.put("ITEM_BOUGHT", dtoItem.isItemBought());
+
+        String id = "ITEM_ID=?";
+        String[] args = {dtoItem.getItemId()+""};
+
+        return getWritableDatabase().update(TABLE, values, id, args);
+    }
+
+    public long deleteItem(int idItem){
+        String id = "ITEM_ID=?";
+        String[] args = {idItem+""};
+
+        return getWritableDatabase().delete(TABLE, id, args);
     }
 
     public int clearList(){
