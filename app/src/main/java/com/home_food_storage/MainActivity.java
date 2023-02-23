5ec12f6ea1f639ce7ff310969797760a4dae0e6e
Matheus Carvalho <matheuso.c08@gmail.com>
2023-02-23 18:05:12 -0300

    Add a 'Toast' when click on the button 'sendStorage' to ask if the user is sure to insert the bought items in the storage base. Implemented an function from 'RecyclerItemClickListener.java' that allow to select an item from the 'RecyclerView' and send the data to the EditItemAcitivity

--- app/src/main/java/com/home_food_storage/MainActivity.java
+++ app/src/main/java/com/home_food_storage/MainActivity.java
@@ -1,12 +1,15 @@
 package com.home_food_storage;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ import android.widget.Toast;
 
 import com.home_food_storage.dao.DaoItemList;
 import com.home_food_storage.dto.DtoItem;
+import com.home_food_storage.shopping_list.EditItemActivity;
 import com.home_food_storage.shopping_list.ItemListAdapter;
 import com.home_food_storage.shopping_list.NewItemActivity;
 
@@ -25,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
     TextView textViewMessageEmptyList;
     RecyclerView recyclerViewItemList;
     Button buttonNewItem, buttonSendStorage, buttonCleanList;
-    ArrayList<DtoItem> arrayItemList = new ArrayList<>();
+    ArrayList<DtoItem> arrayItemList;
     DaoItemList daoItemList = new DaoItemList(MainActivity.this);
+    DtoItem dtoItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +60,53 @@ public class MainActivity extends AppCompatActivity {
                 updateList();
             }
         });
+
+        buttonSendStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder msg = new AlertDialog.Builder(MainActivity.this);
+                msg.setMessage("Tem certeza que deseja incluir os itens ao estoque?");
+                msg.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Os itens comprados foram incluídos no estoque.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                msg.setNegativeButton("Não", null);
+                msg.show();
+            }
+        });
+        
+        recyclerViewItemList.addOnItemTouchListener(new RecyclerItemClickListener(MainActivity.this, recyclerViewItemList,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(MainActivity.this, "Clicou!", Toast.LENGTH_SHORT).show();
+
+                        dtoItem = arrayItemList.get(position);
+
+                        Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+                        intent.putExtra("id", dtoItem.getItemId());
+                        intent.putExtra("name", dtoItem.getItemName());
+                        intent.putExtra("description", dtoItem.getItemDescription());
+                        intent.putExtra("weight", dtoItem.getItemWeight()+"");
+                        intent.putExtra("price", dtoItem.getItemPrice()+"");
+                        intent.putExtra("quantity", dtoItem.getItemQuantity()+"");
+                        intent.putExtra("bought", dtoItem.isItemBought());
+                        startActivity(intent);
+                    }
+        
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+                        Toast.makeText(MainActivity.this, "Segurooou", Toast.LENGTH_SHORT).show();
+        
+                    }
+        
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        
+                    }
+        }));
 
         updateList();
     }
